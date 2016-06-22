d8e7b3ad24adbadce7f310d970054a3746a0d8f8
Michael Tobin <mikel.tobin@gmail.com>
2016-06-22 11:13:52 -0600

    Replaced chats data with hole # and par in services.js. Added buttons to the main page. IE the default load page tab-dash.html. Added map div to chat-detail.html and working on getting it loaded

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,131 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'Hole 1',
+    par: 'Par: 4',
+    face: 'img/ben.png',
+    lat:'',
+    long:''
+
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: 'Hole 2',
+    par: 'Par: 5',
+    face: 'img/max.png',
+    lat:'',
+    long:''
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    name: 'Hole 3',
+    par: 'Par: 3',
+    face: 'img/adam.jpg',
+    lat:'',
+    long:''
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    name: 'Hole 4',
+    par: 'Par: 4',
+    face: 'img/perry.png',
+    lat:'',
+    long:''
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: 'Hole 5',
+    par: 'Par: 5',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 5,
+    name: 'Hole 6',
+    par: 'Par: 4',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 6,
+    name: 'Hole 7',
+    par: 'Par: 3',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 7,
+    name: 'Hole 8',
+    par: 'Par: 4',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 8,
+    name: 'Hole 9',
+    par: 'Par: 4',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 9,
+    name: 'Hole 10',
+    par: 'Par: 3',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 10,
+    name: 'Hole 11',
+    par: 'Par: 5',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 11,
+    name: 'Hole 12',
+    par: 'Par: 4',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 12,
+    name: 'Hole 13',
+    par: 'Par: 3',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 13,
+    name: 'Hole 14',
+    par: 'Par: 4',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 14,
+    name: 'Hole 15',
+    par: 'Par: 4',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 15,
+    name: 'Hole 16',
+    par: 'Par: 5',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 16,
+    name: 'Hole 17',
+    par: 'Par: 4',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
+  }, {
+    id: 17,
+    name: 'Hole 18',
+    par: 'Par: 4',
+    face: 'img/mike.png',
+    lat:'',
+    long:''
   }];
 
   return {
